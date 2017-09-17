ddf43a0ee07a58f8f4303268d43e522f544e52c8
Lizzie <ematusov@redhat.com>
2017-09-16 18:23:33 -0700

    Added Core and Selector Files

---
+++ airQ/src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { APIModule } from './data-access/api.service.module';
+
+export { APIService }  from './data-access/api.service';
+
+export * from './interfaces';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+      ],
+  exports: [],
+  declarations: []
+})
+export class CoreModule { }
---
+++ airQ/src/app/core/model/airdata.ts
@@ -0,0 +1,3 @@
+export class AirData {
+  testdata: string;
+}
